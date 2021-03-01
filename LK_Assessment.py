c94376025b33563cc46259eade69e02e32c0709d
Gestina <gyassa4@gmail.com>
2021-02-28 21:27:43 -0800

    Add single_record query compatibility. Add corresponding tests

--- LK_Assessment.py
+++ LK_Assessment.py
@@ -4,12 +4,38 @@
 
 import shotgun_api3
 from shotgun_api3 import Shotgun
-
 from flask import Flask
 app = Flask(__name__)
 
 sg = Shotgun("https://laika-demo.shotgunstudio.com",
              'code_challenge', '$zvMznkhddo0tgwgwbftzaqob')
+
+
+@app.route('/lk/project/<projectID>/shots')
+def shots(projectID):
+    """ Renders table of sg_latest_version all shots in given projectID"""
+
+    filters = [['project', 'is', {'type': 'Project', 'id': int(projectID)}]]
+    shots = sg.find('Shot', filters)
+
+    html = '''<table>
+        <thead>
+            <tr>
+                <td>shot_id</td>
+                <td>sg_latest_version</td>
+            </tr>
+        </thead>
+        <tbody>
+        '''
+    for shot in shots:
+        html += '<tr>'
+        html += '<td>' + str(shot['id']) + '</td>\n'
+        html += '<td>' + \
+            str(run_query(shot, 'sg_latest_version')) + '</td>\n'
+        html += '<tr>'
+    html += '<tbody>\n</table>'
+
+    return html
 
 
 @app.route('/lk/project/<projectID>/sequences')
@@ -69,6 +95,20 @@ def filters_from_schema(conditions, parent):
     return filters
 
 
+def order_from_schema(Query_Props):
+    """ Constructs order list based on the conditions in a schema_field_read() call
+
+            Parameters:
+                Query_Props: Properties from a schema_field_read() call
+
+            Returns: Order list to sort by
+
+    """
+
+    return [{'field_name': Query_Props['summary_value']['value']['column'],
+             'direction': Query_Props['summary_value']['value']['direction']}]
+
+
 def run_query(entity_obj, schema_fname):
     """ Evaluates and returns the result of a Shotgun query field without knowing its filter conditions
 
@@ -81,11 +121,18 @@ def run_query(entity_obj, schema_fname):
     """
     Query_Props = sg.schema_field_read(entity_obj['type'], schema_fname)[
         schema_fname]['properties']
+
     filters = filters_from_schema(
         Query_Props['query']['value']['filters']['conditions'], entity_obj)
     filter_operator = 'all'
     summary_fields = [{'field': Query_Props['summary_field']
                        ['value'], 'type': Query_Props['summary_default']['value']}]
+
+    # single_record not a valid summary type, use sg.find_one()
+    if (Query_Props['summary_default']['value']) == 'single_record':
+        # query field sort order
+        order_list = order_from_schema(Query_Props)
+        return sg.find_one(Query_Props['query']['value']['entity_type'], filters, fields=None, order=order_list)
     return sg.summarize(Query_Props['query']['value']['entity_type'], filters, summary_fields, filter_operator)['summaries']
 
 
--- LK_Assessment.test.py
+++ LK_Assessment.test.py
@@ -1,5 +1,5 @@
 import unittest
-from LK_Assessment import filters_from_schema
+from LK_Assessment import filters_from_schema, order_from_schema
 
 
 parent = {'type': 'Sequence', 'id': 41}
@@ -29,9 +29,32 @@ conditions3 = [{'path': 'entity.Shot.sg_sequence',
                             'name': 'Current Sequence',
                             'valid': 'parent_entity_token'}]}]
 
+order1 = {'default_value': {''},
+          'query': {''},
+          'summary_default': {'editable': False,
+                              'value': 'single_record'},
+          'summary_field': {'editable': False,
+                            'value': 'code'},
+          'summary_value': {'editable': False,
+                            'value': {'column': 'TEST_COL',
+                                      'detail_link': True,
+                                      'direction': 'desc'}}}
+
+order2 = {'default_value': {''},
+          'query': {''},
+          'summary_default': {'editable': False,
+                              'value': 'single_record'},
+          'summary_field': {'editable': False,
+                            'value': 'code'},
+          'summary_value': {'editable': False,
+                            'value': {'column': 'TEST_COL_2',
+                                      'detail_link': True,
+                                      'direction': 'asc'}}}
+
 
 class TestFilterConversion(unittest.TestCase):
 
+    # test filters
     def test_cond1(self):
         self.assertEqual(filters_from_schema(conditions1, parent),
                          [['entity.Shot.sg_sequence', 'is', parent], ['entity.Shot.sg_status_list', 'is_not', 'fin']])
@@ -43,6 +66,15 @@ class TestFilterConversion(unittest.TestCase):
     def test_cond3(self):
         self.assertEqual(filters_from_schema(conditions3, parent), [
                          ['entity.Shot.sg_sequence', 'is', parent]])
+
+    # test ordering
+    def test_order1(self):
+        self.assertEqual(order_from_schema(order1), [
+                         {'field_name': 'TEST_COL', 'direction': 'desc'}])
+
+    def test_order2(self):
+        self.assertEqual(order_from_schema(order2), [
+                         {'field_name': 'TEST_COL_2', 'direction': 'asc'}])
 
 
 if __name__ == '__main__':
